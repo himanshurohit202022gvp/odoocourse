b213fbe95c75e74143568485327ca24412277d56
“himanshurohit202022gvp” <himanshurohit636@gmail.com>
2021-12-20 00:53:39 +0530

    With excerciseMArble shope details

---
+++ custom/marble/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Marble Data',
+    'category': 'Sales',
+    'application': True,
+    'data' : [
+            'security/ir.model.access.csv',
+            'views/marble_menus.xml',
+            'views/marble_views.xml'
+        ],
+
+}
